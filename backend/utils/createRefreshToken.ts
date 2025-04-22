9d415b10b810c047559ccbc4482d842ba4d4dc94
Abdalrhman yasser <abdoarfat2006@gmail.com>
2025-04-22 17:46:49 +0200

    Update createRefreshToken.ts

--- backend/utils/createRefreshToken.ts
+++ backend/utils/createRefreshToken.ts
@@ -5,7 +5,7 @@ import { REFRESH_TOKEN_SECRET } from "../constants/envVariable.js";
 const createRefreshToken = async (user: TTokenPayload) => {
   try {
     const token = await jwt.sign(user, REFRESH_TOKEN_SECRET as string, {
-      expiresIn: "20m",
+      expiresIn: "7d",
     });
     return token;
   } catch (error) {
